9c19bc9638571e500a451f86976cc3fb2eae11a1
Akash Trivedi <akash@citizencode.io>
2019-05-01 23:05:59 -0700

    connect client side to server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.post('/', function (req, res) {
+  res.send(JSON.stringify('successful request my dude!'))
+})
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
